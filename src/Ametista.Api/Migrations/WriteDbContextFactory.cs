4438b1b7ee572121d7547bbb06fe67e57340e9dd
max.pavlov <max@radacode.com>
2019-11-14 10:22:41 +0200

    Porting o postgres

--- src/Ametista.Api/Migrations/WriteDbContextFactory.cs
+++ src/Ametista.Api/Migrations/WriteDbContextFactory.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using Ametista.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Ametista.Api.Migrations
 {
@@ -11,9 +14,42 @@ namespace Ametista.Api.Migrations
     {
         public WriteDbContext CreateDbContext(string[] args)
         {
-            var connectionString = Program.Configuration.GetConnectionString("MigrationConnection");
+            var connectionString = Program.Configuration.GetConnectionString("SqlConnectionString");
+
+            ILogger logger = Program.Configuration.Get<ILogger>();
+            
+            logger.LogInformation("Example log message");
 
             return new WriteDbContext(connectionString);
+        }
+        
+        IConfiguration GetAppConfiguration()
+        {
+            var environmentName =
+                Environment.GetEnvironmentVariable(
+                    "DOTNETCORE_ENVIRONMENT");
+
+            var dir = Directory.GetParent(AppContext.BaseDirectory);      
+
+            if(EnvironmentName.Development.Equals(environmentName, 
+                StringComparison.OrdinalIgnoreCase))
+            {                  
+                var depth = 0;
+                do
+                    dir = dir.Parent;
+                while (++depth < 5 && dir.Name != "bin");
+                dir = dir.Parent;
+            }
+
+            var path = dir.FullName;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environmentName}.json", true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
         }
     }
 }
